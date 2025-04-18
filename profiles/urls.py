13c2176b3012c9b5fbba190d18b918fa278a27bf
unknown <manviraghu357@gmail.com>
2025-04-18 18:12:36 +0530

    profile view updated

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile_view'),
     path('update_profile/', views.update_profile, name='update_profile'),
     path('delete_profile/', views.delete_profile, name='delete_profile'),
-    path('bookings/', views.bookings_view, name='bookings_view'),
-    path('cancel_booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+    path('cancel_booking/', views.cancel_booking, name='cancel_booking'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from flights.models import Flights
+from django.http import HttpResponse
+
 
 # Create your views here.
+@login_required(login_url='users:login_redirect')
 def profile_view(request):
-    return render(request, 'profiles/test.html',{'message': 'Profile View'})    
+    user = request.user  # Get the currently logged-in user
+    flights = Flights.objects.filter(passengers__user=user)  # Retrieve flights associated with the user
+    return render(request, 'profiles/profile.html', {
+        'message': 'Profile View',
+        'flights': flights
+    })
 
 def update_profile(request):
-    pass
+    return HttpResponse('Update Profile View will be updated soon')
+
 def delete_profile(request):
     pass
-def bookings_view(request):
-    pass
-def cancel_booking(request, booking_id):
-    pass
+
+def cancel_booking(request):
+    return render(request, 'profiles/cancel_booking.html', {
+        'message': 'Cancel Booking View will be updated soon'
+    })
